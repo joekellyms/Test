6b41f5e1092b74f071baab1f65eaa513660ca430
Joe Kelly <joekelly@microsoft.com>
2016-03-10 11:05:24 -0800

    Added files via upload

---
+++ AudioTester.cs
@@ -0,0 +1,26 @@
+﻿//
+// Copyright (C) Microsoft. All rights reserved.
+//
+
+using UnityEngine;
+using UAudioTools;
+
+public class AudioTester : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject testEmitter;
+    [SerializeField]
+    private string eventName;
+    [SerializeField]
+    private RoomSize updateRoom;
+
+	private void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.F))
+		{
+            UAudioManager.Instance.SetAllRoomModels(this.updateRoom, true);
+            UAudioManager.Instance.PlayEvent(this.eventName, this.testEmitter);
+            //UAudioManager.Instance.PlayEvent("Test");
+        }
+	}
+}
